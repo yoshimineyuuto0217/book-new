ea3a6d02bb9d0305d92da1b91b0a2d4b2e761803
吉嶺勇斗 <jilingyongdou@gmail.com>
2024-12-23 22:48:48 +0900

    名前を変更しました

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read scope
           </a>
         </div>
       </main>
